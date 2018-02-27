dd73f300ef7a227d7b335b3e178f96217b208d24
Unknown <judit_7492@hotmail.com>
2018-02-27 16:49:37 +0100

    canvi form controller

--- src/pets/WebBundle/Controller/FormsController.php
+++ src/pets/WebBundle/Controller/FormsController.php
@@ -3,13 +3,14 @@
 namespace pets\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Entity\Usuaris;
 
-class FormController extends Controller
+class FormsController extends Controller
 {
     public function createAction()
     {
         $titol = "Form usuaris"; 
-        $usuari = new Usuari(); 
+        $usuari = new Usuaris(); 
         $form = $this->createForm(UsuarisType::class, $usuari); 
         return $this->render('petsWebBundle:Forms:formUsuaris.html.twig', array( 'titol' => $titol, 'form' =>$form->createView() ));
     }
