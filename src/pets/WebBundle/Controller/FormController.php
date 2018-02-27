47e2407bb6ff262e191c6727ad0412b71ec42846
Unknown <victor.auyanet@gmail.com>
2018-02-27 16:34:55 +0100

    Form-Views 1

--- src/pets/WebBundle/Controller/FormController.php
+++ src/pets/WebBundle/Controller/FormController.php
@@ -4,7 +4,7 @@ namespace pets\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class FormController extends Controller
 {
     public function createAction()
     {
