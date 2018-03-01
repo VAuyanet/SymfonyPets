bf284b752639946cc57ad8e208027241c917cb2e
Unknown <judit_7492@hotmail.com>
2018-03-01 15:16:11 +0100

    Subtasques type

--- src/pets/WebBundle/Form/SubtasquesType.php
+++ src/pets/WebBundle/Form/SubtasquesType.php
@@ -5,6 +5,11 @@ namespace pets\WebBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class SubtasquesType extends AbstractType
 {
@@ -13,7 +18,7 @@ class SubtasquesType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('titol')->add('descripcio')->add('dataInici')->add('dataFinal')->add('prioritat')->add('usuaris')->add('idTasca');
+        $builder->add('titol', TextType::class)->add('descripcio', TextType::class)->add('dataInici', DateType::class)->add('dataFinal', DateType::class)->add('prioritat', TextType::class)->add('usuaris', TextType::class)->add('idTasca', SubmitType::class);
     }/**
      * {@inheritdoc}
      */
