bb6c287a03e43221048bab3be448b33a932b267d
Unknown <judit_7492@hotmail.com>
2018-03-02 15:36:25 +0100

    Form login

---
+++ src/pets/WebBundle/Controller/FormsLoginController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace pets\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use pets\WebBundle\Form\LoginType;
+use pets\WebBundle\Entity\Usuaris;
+use Symfony\Component\HttpFoundation\Request;
+
+class FormsLoginController extends Controller
+{
+     public function searchLoginAction(Request $request)
+    {
+        $usuari = new Usuaris(); 
+        $form = $this->createForm(LoginType::class, $usuari);
+        
+        $form->handleRequest($request);
+        if($form->isValid()){
+            $titolForm = "Login";
+            $status = "Formulari vàlid";
+            $data = array(
+            'email' => $form->get('email')->getData(),
+            'password' => $form->get('password')->getData(),
+            );
+        } else{
+            $titolForm = null;
+            $status = null;
+            $data = null;
+        }
+         
+        $estat="";
+         if($form->isValid()){
+             //cridem a l'entity manager
+             $em = $this->getDoctrine()->getManager();
+             //crearem el repositori de tasques
+             $usuaris_repo = $em->getRepository("petsWebBundle:Usuaris");
+             //cerquem la tasca per id
+             $id = $form->get('id_usuari')->getData();
+             $tasca = $tasques_repo->find($id);
+             
+             if($usuari != null){
+                 $estat = "Usuari trobat";
+             }else{  
+                 $estat = "Usuari no trobat";
+             }
+            
+        
+        }
+        return $this->render('petsWebBundle:Forms:formLogin.html.twig', array( 'status' => $status, 'data' => $data, 'titol' => $titolForm, 'form' =>$form->createView(), 'estat' => $estat  ));
+        
+    }
+    
+    
+    
+}
+
+
+
---
+++ src/pets/WebBundle/Form/LoginType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace pets\WebBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+//TextType, TextareaType, SubmitType, etc. 
+class LoginType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('email', EmailType::class)->add('password', TextType::class)->add('Login', SubmitType::class);       
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'pets\WebBundle\Entity\Usuaris'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'pets_webbundle_usuaris';
+    }
+
+
+}
