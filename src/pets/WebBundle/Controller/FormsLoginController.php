0d461f19811782dbd8f93e2e5a317f5abf4287a5
Unknown <judit_7492@hotmail.com>
2018-03-05 19:21:29 +0100

    form login

--- src/pets/WebBundle/Controller/FormsLoginController.php
+++ src/pets/WebBundle/Controller/FormsLoginController.php
@@ -9,50 +9,43 @@ use Symfony\Component\HttpFoundation\Request;
 
 class FormsLoginController extends Controller
 {
-     public function searchLoginAction(Request $request)
+    public function loginAction(Request $request)
     {
         $usuari = new Usuaris(); 
         $form = $this->createForm(LoginType::class, $usuari);
-        
+        $estat="";
+        $status="";
+        $data="";
+        $titol="";
         $form->handleRequest($request);
         if($form->isValid()){
-            $titolForm = "Login";
-            $status = "Formulari vàlid";
-            $data = array(
-            'email' => $form->get('email')->getData(),
-            'password' => $form->get('password')->getData(),
-            );
-        } else{
-            $titolForm = null;
-            $status = null;
-            $data = null;
-        }
-         
-        $estat="";
-         if($form->isValid()){
-             //cridem a l'entity manager
-             $em = $this->getDoctrine()->getManager();
-             //crearem el repositori de tasques
-             $usuaris_repo = $em->getRepository("petsWebBundle:Usuaris");
-             //cerquem la tasca per id
-             $id = $form->get('idUsuari')->getData();
-             $tasca = $tasques_repo->find($id);
-             
-             if($usuari != null){
-                 $estat = "Usuari trobat";
-             }else{  
-                 $estat = "Usuari no trobat";
-             }
-            
-        $form = $this->createForm(LoginType::class, $usuari);
+            $email = $form->get('email')->getData();
+            $password = $form->get('password')->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $usuaris_repo = $em->getRepository("petsWebBundle:Usuaris");
+            $id = $form->get('idUsuari')->getData();
+            //$usuari = $usuaris_repo->find($id);
+            $usuari = $usuaris_repo->findOneBy([
+                'username' => $username,
+                'password' => $password,
+            ]);
+            if($usuari != null){
+                return $this->redirectToRoute('pets_web_formTasquesCreate');
+                $estat ="login correcte";
+            }else{  
+                return $this->render('petsWebBundle:Forms:formLogin.html.twig', array( 'status' => $status, 'data' => $data, 'titol' => $titolForm, 'form' =>$form->createView(), 'estat' => $estat  ));
+            }
+
+            $form = $this->createForm(LoginType::class, $usuari);
 
         }
-        return $this->render('petsWebBundle:Forms:formLogin.html.twig', array( 'status' => $status, 'data' => $data, 'titol' => $titolForm, 'form' =>$form->createView(), 'estat' => $estat  ));
-        
+        return $this->render('petsWebBundle:Forms:formLogin.html.twig', array( 'status' => $status, 'data' => $data, 'titol' => $titol, 'form' =>$form->createView(), 'estat' => $estat  ));
+
     }
-    
-    
-    
+
+
+
 }
 
 
